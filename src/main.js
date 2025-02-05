488a7044f8d0dadcc465531b778cf0fa63674bc7
Julian <bleeckerj@gmail.com>
2025-02-04 18:44:46 -0800

    And sends something back into the Editor

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 
-// import { Editor } from '@tiptap/core'
-// import StarterKit from '@tiptap/starter-kit'
-import { Editor } from 'https://esm.sh/@tiptap/core'
-import StarterKit from 'https://esm.sh/@tiptap/starter-kit'
+import { Editor } from '@tiptap/core'
+import StarterKit from '@tiptap/starter-kit'
+// import { Editor } from 'https://esm.sh/@tiptap/core'
+// import StarterKit from 'https://esm.sh/@tiptap/starter-kit'
 const { invoke } = window.__TAURI__.core;
 
 let greetInputEl;
@@ -10,7 +10,12 @@ let greetMsgEl;
 
 async function greet() {
   // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-  greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
+  //greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
+  invoke("greet", { name: greetInputEl.value }).then((res) => {
+    greetMsgEl.textContent = res;
+    console.log(editor);
+    editor.chain().focus().insertContent('Hello World from Rust Backend '+greetInputEl.value+'<').run()
+  });
 }
 
 window.addEventListener("DOMContentLoaded", () => {
@@ -23,8 +28,8 @@ window.addEventListener("DOMContentLoaded", () => {
 });
 
 
-new Editor({
+const editor = new Editor({
   element: document.querySelector('.element'),
   extensions: [StarterKit],
-  content: '<p>Hello World!</p>',
+  content: '<p>Hello World! This is the Editor</p>',
 })
